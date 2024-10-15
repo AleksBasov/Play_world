dff563032996bd305df43453e57c1a4cac7edede
AleksBasov <basovaleks17s@mail.ru>
2024-10-15 14:48:23 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+
+function burgerMenu(selector) {
+    const menu = document.querySelector(selector);
+    const button = menu.querySelector('.burger-menu_button');
+    const links = menu.querySelectorAll('.burger-menu_link');
+    const overlay = menu.querySelector('.burger-menu_overlay');
+
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', () => toggleMenu());
+    });
+
+    overlay.addEventListener('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.classList.toggle('burger-menu_active');
+
+        if (menu.classList.contains('burger-menu_active')) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'visible';
+        }
+    }
+}
+
+burgerMenu('.burger-menu');
+
+
+
+
+// Gasp animate
+const image = document.getElementById('image');
+const image2 = document.getElementById('image2');
+
+function swing() {
+    gsap.to(image, { rotation: 9, duration: 0.5, yoyo: true, repeat: 1 });
+
+    gsap.to(image2, { 
+        y: -14,
+        duration: 0.5,
+        yoyo: true,
+        repeat: 1,
+        ease: "power1.inOut" 
+     });
+}
+
+// Запускаем анимацию каждые 2 секунды
+setInterval(swing, 2000);
+
+
+
+
+
+
