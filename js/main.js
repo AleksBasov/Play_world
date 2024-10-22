77784eab237efb20c1adc185efc59a5b6ec02170
AleksBasov <basovaleks17s@mail.ru>
2024-10-22 14:12:44 +0300

    NewDesing-pages

--- js/main.js
+++ js/main.js
@@ -66,11 +66,6 @@ setInterval(swing, 2000);
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
